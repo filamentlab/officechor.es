d692439fcfaa6c9c40756eaa5b5ecd29eaba7a83
Stella Kim <stella.kim@icloud.com>
2015-07-17 18:25:43 -0400

    adding fixture adapter; board model fixture data

--- frontend/app/models/board.js
+++ frontend/app/models/board.js
@@ -1,7 +1,15 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  expired: DS.attr('date'),
+  expiryDate: DS.attr('date'),
   isRandom: DS.attr('boolean')
   // todo: add has_many rel to chores
+}).reopenClass({
+	FIXTURES: [
+		{
+			id: 1,
+			expiryDate: 'Fri, 31 Jul 2015 10:10:10 GMT',
+			isRandom: true
+		}
+	]
 });
